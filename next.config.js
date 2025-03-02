41cd3e37610cfb6c06858cfd04540d753887eac4
Igor Peric <igorperic@live.com>
2025-03-02 18:35:58 +0100

    Fix: Remove swcMinify from Next.js config and add avatar placeholder

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['ui-avatars.com'], // Allow images from UI Avatars service
   },
